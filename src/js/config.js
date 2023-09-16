0e0b2661c9dc94cdb4ed91dcb9b718c40431308f
syedfaheem27 <syedfaheem2719@gmail.com>
2023-09-16 15:57:56 +0530

    Change the variable value in config

--- src/js/config.js
+++ src/js/config.js
@@ -2,4 +2,4 @@ export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = "3bfc1a95-8be7-4890-a0c9-dfaf3854453d";
-export const CLOSE_MODAL_SEC = 3;
+export const CLOSE_MODAL_SEC = 2;
